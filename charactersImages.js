aaa64361d856fdc520b24451269b08588d318d32
edianibarrola <edianibarrola@gmail.com>
2023-08-24 13:36:23 +0000

    push

---
+++ charactersImages.js
@@ -0,0 +1,26 @@
+  const charactersImages = {
+    echo: "https://res.cloudinary.com/petrep/image/upload/v1692831347/echo_cwn4qu.png",
+    zuark:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802930/zuark_razx6x.png",
+    zhenwu:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802925/zhenwu_m81rlr.png",
+    ava: "https://res.cloudinary.com/petrep/image/upload/v1692802929/ava_zm3nck.png",
+    axenthon:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802925/axenthon_fssnf5.png",
+    robot:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802926/robotdog_ohsubw.png",
+    zhalia:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802925/zhalia_nimfel.png",
+    veran:
+      "https://res.cloudinary.com/petrep/image/upload/v1692804287/veran_nfkb74.png",
+    sekhmet:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802925/sekhmet_vnaslu.png",
+    binru:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802927/binru_uzkwsp.png",
+    kazon:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802923/kazon_vpiakd.png",
+    zerrok:
+      "https://res.cloudinary.com/petrep/image/upload/v1692802930/zerrok_rkw5d9.png",
+    xaezor:
+      "https://res.cloudinary.com/petrep/image/upload/v1692828125/xaezor_teppem.png",
+  };
